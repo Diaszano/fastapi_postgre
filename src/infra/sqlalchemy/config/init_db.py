ddf292480f39f6ac9f4b25e2cbd7243812b450e1
Diaszano <lucasdiiassantos@gmail.com>
2022-07-05 01:59:59 -0300

    inicialmente funcional

---
+++ src/infra/sqlalchemy/config/init_db.py
@@ -0,0 +1,26 @@
+#-----------------------
+# BIBLIOTECAS
+#-----------------------
+from asyncio import run
+from src.infra.sqlalchemy.models.models import Base
+from src.infra.sqlalchemy.config.connection import engine
+#-----------------------
+# CONSTANTES
+#-----------------------
+#-----------------------
+# CLASSES
+#-----------------------
+#-----------------------
+# FUNÇÕES()
+#-----------------------
+async def create_database():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.drop_all);
+        await connection.run_sync(Base.metadata.create_all);
+
+if(__name__ == "__main__"):
+    run(create_database())
+#-----------------------
+# Main()
+#-----------------------
+#-----------------------
---
+++ src/infra/sqlalchemy/models/models.py
@@ -0,0 +1,43 @@
+#-----------------------
+# BIBLIOTECAS
+#-----------------------
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+#-----------------------
+# CONSTANTES
+#-----------------------
+Base = declarative_base();
+#-----------------------
+# CLASSES
+#-----------------------
+class User(Base):
+    __tablename__ = "user";
+    
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    );
+    name      = Column(String);
+    favorites = relationship(
+        "Favorite",
+        backref="user"
+    );
+
+class Favorite(Base):
+    __tablename__ = "favorite";
+    
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    );
+    symbol  = Column(String);
+    user_id = Column(Integer,ForeignKey('user.id'));
+#-----------------------
+# FUNÇÕES()
+#-----------------------
+#-----------------------
+# Main()
+#-----------------------
+#-----------------------
